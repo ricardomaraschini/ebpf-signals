2b7c722d46358e55cd53d1c7d4fdca5aafda104d
Ricardo Maraschini <rmarasch@kakkerlakken.my.lan>
2022-12-29 19:56:57 +0100

    feat: bail out if running as non root

--- signals/src/main.rs
+++ signals/src/main.rs
@@ -1,16 +1,34 @@
 pub mod emitters;
 
+use log::error;
 use log::info;
+use std::process;
 use tokio::sync::broadcast;
 use tokio::task;
+use users::get_current_uid;
 
 #[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
+async fn main() {
     env_logger::init();
 
+    if get_current_uid() != 0 {
+        error!("running as non root is not supported");
+        process::exit(1);
+    }
+
     let (tx, _) = broadcast::channel(100);
-    let mut bpf = emitters::SignalEmitter::new()?;
-    bpf.attach(tx.clone())?;
+    let mut bpf = match emitters::SignalEmitter::new() {
+        Ok(bpf) => bpf,
+        Err(err) => {
+            error!("unable to load ebpf program: {}", err);
+            process::exit(1);
+        }
+    };
+
+    if let Err(err) = bpf.attach(tx.clone()) {
+        error!("unable to attach to ebpf program: {}", err);
+        process::exit(1);
+    }
 
     let mut join_handles: Vec<task::JoinHandle<()>> = vec![];
     for i in 0..2 {
@@ -30,7 +48,9 @@ async fn main() -> Result<(), anyhow::Error> {
     drop(tx);
 
     for handle in join_handles {
-        handle.await?;
+        if let Err(err) = handle.await {
+            error!("error waiting for thread: {}", err);
+            process::exit(1);
+        }
     }
-    Ok(())
 }

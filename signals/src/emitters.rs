f6940fb07e6b4b3341fea67ad52af46dca953800
Ricardo Maraschini <rmarasch@kakkerlakken.my.lan>
2022-12-29 19:34:27 +0100

    chore: using broadcast channels

--- signals/src/emitters.rs
+++ signals/src/emitters.rs
@@ -9,18 +9,16 @@ use log::debug;
 use log::error;
 use signals_common::Signal;
 use std::mem::size_of;
-use std::sync::mpsc::SyncSender;
-use tokio::task;
+use tokio::sync::broadcast;
 
 pub struct SignalEmitter {
     bpf: Bpf,
-    dst: SyncSender<Signal>,
 }
 
 impl SignalEmitter {
-    // new returns a new Signal that loads a bpf program from disk and loads it
-    // into the kernel. signals are written to the destination channel.
-    pub fn new(dst: SyncSender<Signal>) -> Result<Self, anyhow::Error> {
+    // new returns a new Signal that reads a bpf program from disk and loads it
+    // into the kernel.
+    pub fn new() -> Result<Self, anyhow::Error> {
         #[cfg(debug_assertions)]
         let mut bpf = Bpf::load(include_bytes_aligned!(
             "../../target/bpfel-unknown-none/debug/signals"
@@ -37,26 +35,20 @@ impl SignalEmitter {
         program.load()?;
         program.attach("signal", "signal_generate")?;
 
-        Ok(Self { bpf, dst })
+        Ok(Self { bpf })
     }
 
     // attach starts to read signal events from the kernel and pipe them through the
-    // destination channel. spawns a task per cpu, each task process events from its
+    // provided destination. spawns a task per cpu, each task process events from its
     // own perf array.
-    pub fn attach(&mut self) -> Result<(), anyhow::Error> {
-        let cpus = online_cpus()?;
-        let mut senders: Vec<SyncSender<Signal>> = vec![];
-        for _ in 0..cpus.len() {
-            senders.push(self.dst.clone());
-        }
-
+    pub fn attach(&mut self, dst: broadcast::Sender<Signal>) -> Result<(), anyhow::Error> {
         let signal_struct_size: usize = size_of::<Signal>();
         let mut perf_array = AsyncPerfEventArray::try_from(self.bpf.map_mut("SIGNALS")?)?;
-        for cpu_id in cpus {
+        for cpu_id in online_cpus()? {
             debug!("spawning task for cpu {}", cpu_id);
             let mut parray = perf_array.open(cpu_id, None)?;
-            let txcopy = senders.pop().unwrap();
-            task::spawn(async move {
+            let local_dst = dst.clone();
+            tokio::spawn(async move {
                 debug!("task for cpu awaiting for events {}", cpu_id);
                 let mut buffers = (0..100)
                     .map(|_| BytesMut::with_capacity(signal_struct_size))
@@ -79,7 +71,7 @@ impl SignalEmitter {
                         let buf = &mut buffers[i];
                         let ptr = buf.as_ptr() as *const Signal;
                         let signal = unsafe { ptr.read_unaligned() };
-                        match txcopy.send(signal) {
+                        match local_dst.send(signal) {
                             Ok(_) => continue,
                             Err(err) => error!("failed to send signal internally: {}", err),
                         }
